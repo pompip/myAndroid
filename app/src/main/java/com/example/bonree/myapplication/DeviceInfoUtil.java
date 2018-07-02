537883101ea2d1ef0d4688ca582da683f8524756
rodon557 <546492510@qq.com>
2018-07-02 18:02:58 +0800

    Initial commit

---
+++ app/src/main/java/com/example/bonree/myapplication/DeviceInfoUtil.java
@@ -0,0 +1,31 @@
+package com.example.bonree.myapplication;
+
+import android.text.TextUtils;
+
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.net.SocketException;
+import java.util.Enumeration;
+
+public class DeviceInfoUtil {
+    private static String mIpAddress;
+
+    public static String getIpAddress() {
+        if (TextUtils.isEmpty(mIpAddress)) {
+            try {
+                for (Enumeration<NetworkInterface> en = NetworkInterface.getNetworkInterfaces(); en.hasMoreElements(); ) {
+                    NetworkInterface intf = en.nextElement();
+                    for (Enumeration<InetAddress> enumIpAddr = intf.getInetAddresses(); enumIpAddr.hasMoreElements(); ) {
+                        InetAddress inetAddress = enumIpAddr.nextElement();
+                        if (!inetAddress.isLoopbackAddress()) {
+                            mIpAddress = inetAddress.getHostAddress().toString();
+                        }
+                    }
+                }
+            } catch (SocketException ex) {
+                ex.printStackTrace();
+            }
+        }
+        return mIpAddress;
+    }
+}
